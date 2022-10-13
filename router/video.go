438f392c26037ea9c805e1042d44cddf60e1501a
QvBiLam <qvbilam@163.com>
2022-10-13 18:13:23 +0800

    video

--- router/video.go
+++ router/video.go
@@ -8,8 +8,9 @@ import (
 func InitVideoRouter(Router *gin.RouterGroup) {
 	VideoRouter := Router.Group("video")
 	{
-		VideoRouter.GET("/:id", api.GetVideoDetail)             // 获取视频详情
-		VideoRouter.POST("/certificate", api.CreateUploadVideo) // 创建上传凭证
-		VideoRouter.PUT("/certificate", api.RefreshUploadVideo) // 更新上传凭证
+		VideoRouter.GET("/:id", api.GetVideoDetail)                      // 获取视频详情
+		VideoRouter.GET("/:id/play/certificate", api.GetPlayCertificate) // 获取播放凭证
+		VideoRouter.POST("/upload/certificate", api.CreateUploadVideo)   // 创建上传凭证
+		VideoRouter.PUT("/upload/certificate", api.RefreshUploadVideo)   // 更新上传凭证
 	}
 }
